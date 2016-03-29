7deb7b9fcf546f9e48a2b20e8b44e4d2a88e7d39
jason5110 <jason51102000@gmail.com>
2016-03-29 22:55:39 +0800

    Initial Commit

---
+++ localNotificationObjctive-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  localNotificationObjctive-C
+//
+//  Created by Jason on 3/29/16.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ localNotificationObjctive-CTests/localNotificationObjctive_CTests.m
@@ -0,0 +1,39 @@
+//
+//  localNotificationObjctive_CTests.m
+//  localNotificationObjctive-CTests
+//
+//  Created by Jason on 3/29/16.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface localNotificationObjctive_CTests : XCTestCase
+
+@end
+
+@implementation localNotificationObjctive_CTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
